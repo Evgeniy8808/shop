b538e2e6ef574d07e32d9dd31cbd5da7f79e519b
Evgeniy8808 <40404160+Evgeniy8808@users.noreply.github.com>
2021-01-21 21:32:01 +0200

    admin

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView
+from .models import Product
+from django.views.generic import ListView, DetailView,View
+
+# class mainappView(View):
+#     def __get__(self, instance, owner):
+
+def test_view(request):
+    return render(request, 'base.html', {})
